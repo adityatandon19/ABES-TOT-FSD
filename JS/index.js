72815f0826abfc9befde50f301d11f505377bcde
Aditya Hare Krishna <adityaharekrishna@Adityas-MacBook-Pro-13.local>
2025-08-20 10:56:13 +0530

    self-invoke function in JS

--- JS/index.js
+++ JS/index.js
@@ -1,12 +1,16 @@
-const data = () => {
-    return "Hi, Welcome to FSD";
-};
-console.log(data());
+// const data = () => {
+//     return "Hi, Welcome to FSD";
+// };
+// console.log(data());
 
-const sum = (a, b) => {
-    return a + b;
-};
-console.log(sum(10, 20));
+// const sum = (a, b) => {
+//     return a + b;
+// };
+// console.log(sum(10, 20));
 
-const add = (a, b) => a + b;  // single-line shorthand
-console.log(add(23, 40));
+// const add = (a, b) => a + b;  // single-line shorthand
+// console.log(add(23, 40));
+
+(()=>{
+     console.log("hello, how are you")
+})();
