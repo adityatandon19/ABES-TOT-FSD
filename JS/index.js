c461f7eacc71b4db669355f62884d70accf3a6bf
Aditya Hare Krishna <adityaharekrishna@Adityas-MacBook-Pro-13.local>
2025-08-20 10:45:54 +0530

    functions in JS

---
+++ JS/index.js
@@ -0,0 +1,14 @@
+// let a = 12;
+// let b = "12";
+// if(a===b) {
+//      console.log("Hii....there");
+// }
+// else{
+//      console.log("Welcome to JS");
+// }
+
+function sum(a, b){
+     return a+b;
+}
+let val=sum(20, 20)
+console.log("sum = " + val)
