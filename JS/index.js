d33bc126e9387dc4ce9bea6330fdb4d16c14d93a
Aditya Hare Krishna <adityaharekrishna@Adityas-MacBook-Pro-13.local>
2025-08-20 11:13:23 +0530

    if else and functions in JS

--- JS/index.js
+++ JS/index.js
@@ -11,6 +11,32 @@
 // const add = (a, b) => a + b;  // single-line shorthand
 // console.log(add(23, 40));
 
-(()=>{
-     console.log("hello, how are you")
-})();
+// (()=>{
+//      console.log("hello, how are you")
+// })();
+
+// function greet(msg="hello"){
+//      return "Hi"+msg
+// }
+// console.log(greet("Welcome"))
+
+function selectLanguage(lang) {
+     let data
+     
+     if(lang == "java"){
+          function javaCompiler(){
+               console.log("Java Compiler is being executed")
+          }
+          data = javaCompiler()
+     }
+     else if(lang == "C"){
+          function cCompiler(){
+               console.log("C compiler is bring executed")
+          }
+          data = cCompiler()
+     }
+     else {
+          console.log("Selected language compile is not found")
+     }
+}
+selectLanguage("python")
