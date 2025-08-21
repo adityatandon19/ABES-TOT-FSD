3c437e4dedbabe0dcae7c68bf072e782c502a159
Aditya Hare Krishna <adityaharekrishna@Adityas-MacBook-Pro-13.local>
2025-08-21 11:09:59 +0530

    made changes to JS file as employee record

---
+++ JS/Objects/index.js
@@ -0,0 +1,20 @@
+const employee={
+     ename:"Aditya Tandon",
+     eage:"37",
+     eaddress:"Ghaziabad",
+     planguage:[{
+          lang1: "java",
+          lang2: "python"
+     },
+     {
+          database1: "Oracle",
+          database2: "SQL"
+     },
+     {
+          framework1: "Hibernate",
+          framework2: "Spring"
+     }]
+}
+employee.company="Infosys";
+console.log(employee);
+// console.log(employee.planguage[0].lang1);
