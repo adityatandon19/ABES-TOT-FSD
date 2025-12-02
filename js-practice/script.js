779f54d6f23ec06d3c15eaccc1571687c2e6e055
Aditya Hare Krishna <adityat1904@gmail.com>
2025-12-02 23:09:27 +0530

    added js-practice folder and assignment for CSE students

--- js-practice/script.js
+++ js-practice/script.js
@@ -3,3 +3,22 @@ function addNumbers() {
      let b = Number(document.getElementById("n2").value);
      document.getElementById("sum").innerText = "Sum is: " + (a + b);
 }
+
+function makeRed() {
+     document.getElementById("output").style.color = "red";
+}
+
+function sayHello() {
+     alert("Hello, CSE 13!");
+}
+
+function textLength() {
+     let val = document.getElementById("box").value;
+     document.getElementById("output").innerHTML = "Length is: " + val.length;
+}
+
+function reverseText() {
+     let val = document.getElementById("box").value;
+     let rev = val.split("").reverse().join("");
+     document.getElementById("rev1").innerHTML = "Reversed is: " + rev;
+}
